fd059bfd58ac2f236eab80ce818a347d852aaca5
John Watson <johnwatson484@hotmail.com>
2021-07-09 22:27:44 +0100

    Sessions added

--- app/message-action.js
+++ app/message-action.js
@@ -1,3 +1,8 @@
-module.exports = function (message) {
-  console.log(message.body)
+const config = require('./config')
+const { MessageSender } = require('./messaging')
+
+module.exports = async function (message) {
+  const queueSender = new MessageSender('queue-sender', config.sessionQueueConfig)
+  await queueSender.sendMessage({ body: `Your message was ${message.body}`, sessionId: message.correlationId })
+  await queueSender.closeConnection()
 }
