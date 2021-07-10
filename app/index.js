39132b748b9c1fcae14bc46a376b8e2116992bc6
John Watson <johnwatson484@hotmail.com>
2021-07-10 14:36:07 +0100

    FFC module used

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
 (async function () {
   const config = require('./config')
   const messageAction = require('./message-action')
-  const { MessageReceiver } = require('./messaging')
+  const { MessageReceiver } = require('ffc-messaging')
 
   // send and receive from queue
-  const queueReceiver = new MessageReceiver('queue-receiver', config.queueConfig, messageAction)
+  const queueReceiver = new MessageReceiver(config.queueConfig, messageAction)
+  await queueReceiver.subscribe()
 }())
--- app/message-action.js
+++ app/message-action.js
@@ -1,8 +1,16 @@
 const config = require('./config')
-const { MessageSender } = require('./messaging')
+const { MessageSender } = require('ffc-messaging')
 
 module.exports = async function (message) {
-  const queueSender = new MessageSender('queue-sender', config.sessionQueueConfig)
-  await queueSender.sendMessage({ body: `Your message was ${message.body}`, sessionId: message.correlationId })
-  await queueSender.closeConnection()
+  console.log('Message received:', message.body)
+  const responseMessage = {
+    body: { content: `Your message was ${message.body.content}` },
+    type: 'session test message',
+    subject: 'test',
+    source: 'async backend',
+    sessionId: message.correlationId
+  }
+  const sessionQueueSender = new MessageSender(config.sessionQueueConfig)
+  await sessionQueueSender.sendMessage(responseMessage)
+  await sessionQueueSender.closeConnection()
 }
